e45e7b92dab326f31038a74e36edd776f67aecdb
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-02-20 17:05:40 -0500

    add code to create a chat app

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,19 @@
 'use strict';
+var myFirebaseRef = new Firebase("https://fiery-inferno-7940.firebaseio.com/");
+
+    $('#messageInput').keypress(function (e) {
+      if (e.keyCode == 13) {
+        var name = $('#nameInput').val();
+        var text = $('#messageInput').val();
+        myFirebaseRef.push({name: name, text: text});
+        $('#messageInput').val('');
+      }
+  });
+      myFirebaseRef.on('child_added', function(snapshot) {
+        var message = snapshot.val();
+        displayChatMessage(message.name, message.text);
+      });
+  function displayChatMessage(name, text) {
+    $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+  };
